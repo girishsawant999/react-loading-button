100c44b4a75dab7a2b0f53662eeb33b6d942f39f
Girish Sawant <girishsawant999.gs@gmail.com>
2021-12-25 00:13:49 +0530

    feat: :sparkles: Loading Button Component
    Created a loading button component with loader in it.

---
+++ src/Components/Loaders/CircleNotchSolid.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const CircleNotchSolid = function (props) {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fas"
+      data-icon="circle-notch"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 512 512"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z"
+      >
+        <animateTransform
+          attributeType="xml"
+          attributeName="transform"
+          type="rotate"
+          from="0 256 256"
+          to="360 256 256"
+          dur="1s"
+          additive="sum"
+          repeatCount="indefinite"
+        />
+      </path>
+    </svg>
+  );
+};
+
+export default CircleNotchSolid;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,72 @@
+import propTypes from 'prop-types';
 import React from 'react';
+import { CircleNotchSolid } from './Components/Loaders';
 import './style.scss';
 
-const MainComp = () => {
-  return <div className="red-div">Hello World</div>;
+const When = ({ isTrue, children }) => {
+  if (!isTrue) return null;
+  return children;
 };
 
-export default MainComp;
+const removeProps = (props, ...args) => {
+  const newProps = { ...props };
+  args.forEach((arg) => {
+    delete newProps[arg];
+  });
+  return newProps;
+};
+
+const classes = (...args) => args.join(' ').trim();
+
+const Button = function (props) {
+  const { children, disabled, loading, buttonType, className } = props;
+  const buttonProps = removeProps(
+    props,
+    'children',
+    'disabled',
+    'loading',
+    'buttonType',
+    'className',
+  );
+
+  return (
+    <button
+      disabled={disabled || loading}
+      className={classes(buttonType, className)}
+      {...buttonProps}
+    >
+      <When isTrue={loading}>
+        <span>
+          <CircleNotchSolid />
+        </span>
+      </When>
+      <span>{children}</span>
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: 'button',
+  loading: false,
+  disabled: false,
+  buttonType: 'danger',
+};
+
+Button.propTypes = {
+  children: propTypes.node.isRequired,
+  loading: propTypes.bool,
+  disabled: propTypes.bool,
+  buttonType: propTypes.oneOf([
+    'primary',
+    'secondary',
+    'danger',
+    'success',
+    'warning',
+    'info',
+    'light',
+    'dark',
+    'glass',
+  ]),
+};
+
+export default Button;
